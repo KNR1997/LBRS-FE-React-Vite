a97721adce664a754a398ae601870837a32aa6f2
KNR1997 <kethakaranasinghe@gmail.com>
2023-11-06 19:39:21 +0530

    UI build

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
@@ -20,8 +20,29 @@ const nav__links = [
   },
 ];
 function Header() {
+  const headerRef = useRef(null);
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunc();
+
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  });
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper d-flex align-items-center justify-content-between">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,15 @@ import { Container, Row, Col } from "reactstrap";
 import worldImg from "../assets/images/world.png";
 import heroImg1 from "../assets/images/hero-img01.jpg";
 import heroImg2 from "../assets/images/hero-img02.jpg";
+import experienceImg from "../assets/images/experience.png";
 import Subtitle from "../shared/Subtitle";
 
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedToursList";
+import MasonryImagesGallery from "../components/image-gallery/MasonryImagesGallery"
+import Testimonials from "../components/Testimonial/Testimonial";
+import Newsletter from "../shared/Newsletter";
 
 export default function Home() {
   return (
@@ -81,6 +85,80 @@ export default function Home() {
           </Row>
         </Container>
       </section>
+
+      {/* Experience section */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"}/>
+
+                <h2>
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, cupiditate?
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years Experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg='6'>
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* gallery section */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <Subtitle subtitle={"Gallery"}/>
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg='12'>
+              <MasonryImagesGallery/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* testomonial section */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <Subtitle subtitle={'Fans Love'}/>
+              <h2 className="testimonial__title">What our fans say about us</h2>
+            </Col>
+            <Col lg='12'>
+              <Testimonials/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Newsletter/>
     </>
   );
 }
